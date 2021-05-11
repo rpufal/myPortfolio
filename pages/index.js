fc264c8be46dea99c33b1d36890926861be2613c
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-11 14:53:42 -0300

    Portfolio

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import homePic from '../public/images/home.png';
+
+const Home = () => (
+  <div>
+    <img src={ homePic } alt="home" />
+  </div>
+);
+
+export default Home;
