f5cd1d8bbd970e93b34d04ad95f8c3ebe5b4e9b6
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-13 17:43:56 -0300

    cards de skills prontos estruturalmente

--- components/SkillCard.js
+++ components/SkillCard.js
@@ -4,16 +4,20 @@ import styles from '../styles/Skills.module.css';
 
 const SkillCard = ({ imgSrc, imgAlt, skillTitle, skillDescription, toolsList }) => (
   <div className={ styles.divSkills }>
-    <img src={ imgSrc } width="25%" alt={ imgAlt } />
-    <div className={ styles.skillTitle }>
-      <h2>{skillTitle}</h2>
-      <p>
-        {skillDescription}
-      </p>
+    <div className={ styles.firstHalf }>
+      <img src={ imgSrc } width="25%" alt={ imgAlt } />
+      <div className={ styles.skillTitle }>
+        <h2>{skillTitle}</h2>
+        <p>
+          {skillDescription}
+        </p>
+      </div>
     </div>
-    <div className={ styles.skillList }>
-      <h3>Languages, frameworks and tools</h3>
-      <div>{toolsList.map((tool) => <p key={ tool }>{tool}</p>)}</div>
+    <div className={ styles.secondHalf }>
+      <div className={ styles.skillList }>
+        <h3>Languages, frameworks and tools</h3>
+        <div>{toolsList.map((tool) => <p key={ tool }>{tool}</p>)}</div>
+      </div>
     </div>
   </div>
 );
