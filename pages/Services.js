d135ee796e6f2bd2e15f8dc7791518e26bc9dad1
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-18 14:09:33 -0300

    starting to implement services page

---
+++ pages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+  <section>
+    <div>
+      <h2>What can I bring to you and your team?</h2>
+      <p>I will help you on creating an easy to implement and easy to use application in order to solve your problems. I also bring great communication, workrate and eagerness to learn!</p>
+    </div>
+  </section>;
+};
+
+export default Services;
